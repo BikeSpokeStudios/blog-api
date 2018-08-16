4f43cb4157bf93a7a8bc1c39b87f8a13b1229f59
David Parker <dav1dp0101@1022606.no-reply.drupal.org>
2018-08-16 12:15:44 -0400

    Finished assignment for Blog API Node 1.4.7

--- server.js
+++ server.js
@@ -27,6 +27,12 @@ app.get('/blogposts', (req, res) => {
   res.json(BlogPosts.get());
 });
 
+//Here's if they want to get a single blog post
+app.get('/blogposts/:id', (req, res) => {
+  console.log(`Returning Blog Post ${req.params.id}`);
+  res.json(BlogPosts.get(req.params.id));
+});
+
 app.post('/blogposts', jsonParser, (req, res) => {
   // ensure that there is a title, content, author name,
   // and an optional publication Date
@@ -40,6 +46,39 @@ app.post('/blogposts', jsonParser, (req, res) => {
   }
   post = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
   res.status(201).json(post);
+});
+
+app.delete('/blogposts/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted Blog Post ${req.params.id}`);
+  res.status(204).end();
+});
+
+app.put('/blogposts/:id', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author', 'publishDate'];
+  for (var i = 0; i < requiredFields.length; i++) {
+    if (!(requiredFields[i] in req.body)) {
+      const message = `Request body missing ${requiredFields[i]} field`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  if (req.params.id !== req.body.id) {
+    const message = `Request body id ${req.body.id} doesn't match parameter id ${req.params.id}`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+
+  console.log(`Updating blog post ${req.body.title}`);
+  BlogPosts.update({
+    title: req.body.title,
+    id: req.body.id,
+    author: req.body.author,
+    content: req.body.content,
+    publishDate: req.body.publishDate
+  });
+  res.status(204).end();
 });
 
 app.listen(process.env.PORT || 8080, () => {
