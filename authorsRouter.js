32eb0a8e8cf61c8270cbd9cfdb4d3a878469a3dc
David Parker <dav1dp0101@1022606.no-reply.drupal.org>
2018-10-01 16:57:50 -0400

    Created PUT /authors endpoint

--- authorsRouter.js
+++ authorsRouter.js
@@ -49,4 +49,48 @@ router.post('/', (req, res) => {
 
 });
 
+router.put('/:id', (req, res) => {
+  if ( !req.body.id ) {
+    const message = "The request body must have an id!";
+    console.error(message);
+    return res.status(400).json({ message: message });
+  }
+  if ( !( req.params.id && req.body.id && req.params.id === req.body.id ) ) {
+    const message = "Request body id and path id must match :)";
+    console.error(message);
+    return res.status(400).json({ message: message });
+  }
+
+  if ( req.body.userName ) {
+    Author.findOne( {userName: req.body.userName} )
+    .then(author => {
+      if (author) {
+        const message = `Username ${req.body.userName} already exists`;
+        console.log(message);
+        res.status(400).json({ message: message });
+      }
+    })
+  }
+
+  const toUpdate = {};
+  const upDateableFields = ["firstName", "lastName", "userName"];
+
+  upDateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Author.findByIdAndUpdate( req.params.id, { $set: toUpdate }, { new: true } )
+  .then(author => res.status(200).json({
+    _id: author._id,
+    name: `${author.firstName} ${author.lastName}`,
+    userName: author.userName
+  }))
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  });
+});
+
 module.exports = router;
